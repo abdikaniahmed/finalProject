8f05d08e30283feea04dbfcb67e00b0d21c7d3bf
abdikaniahmed <cabdiqanisoomaal@gmail.com>
2020-06-08 18:56:39 +0300

    final project comment Fpelearning

---
+++ application/views/backend/footer.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+<footer class="main">
+	<strong> <?php echo get_settings('system_name'); ?> </strong> | &copy; 2020,
+	<a href="<?php echo get_settings('footer_link'); ?>"
+    	target="_blank"><?php echo get_settings('footer_text'); ?></a>
+</footer>
+
+<!--  Number origin +143-52-9933631 -->
